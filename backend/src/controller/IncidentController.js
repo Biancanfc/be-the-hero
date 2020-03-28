9f1e94c5cee525bc2ac3f04293e32b26bfbe12e3
biancanf <bianca.nunes@redfox.tech>
2020-03-28 19:15:11 -0300

    frontend

--- backend/src/controller/IncidentController.js
+++ backend/src/controller/IncidentController.js
@@ -51,7 +51,7 @@ module.exports = {
         const ong_id = req.headers.authorization;
 
         const incident = await connection('incidents').where('id', id).select('ong_id').first();
-
+        
         if (incident.ong_id !== ong_id) {
             return res.status(401).json({
                 error: 'Operation not permitted.'
